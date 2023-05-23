9735c154ecb02e818d711ef54549167a7cb41f44
AndreyTsedrik <awesome.foxe@yandex.ru>
2023-05-23 12:57:25 +0300

    Task

---
+++ Task/Task1.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Введите a, b и длинну массива:");
+int a = InputNumbers("Введите a: ");
+int b = InputNumbers("Введите b: ");
+int langth = InputNumbers("Введите длинну массива");
+
+int[,] array = new int[a, b];
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine("");
+SortingArray(array);
+PrintArray(array);
+
+void SortingArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(langth);
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
---
+++ Task/Task2.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("Введите a, b и длинну массива:");
+int a = InputNumbers("Введите a: ");
+int b = InputNumbers("Введите b: ");
+int langth = InputNumbers("Введите длинну массива: ");
+
+int[,] array = new int[a, b];
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine("");
+
+int minSum = 0;
+int Sum = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int SumLine = SumLineElements(array, i);
+  if (Sum > SumLine)
+  {
+    Sum = SumLine;
+    minSum = i;
+  }
+}
+
+Console.WriteLine($"{minSum+1} - строкa с наименьшей суммой элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(langth);
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
